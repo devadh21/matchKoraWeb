2f6691319914117ad5d677ba38cc359cdce6ab45
devadh21 <devadh21@gmail.com>
2026-07-13 03:29:28 +0100

    update video

--- src/components/ui/VideoPresetation.tsx
+++ src/components/ui/VideoPresetation.tsx
@@ -1,7 +1,7 @@
 export function VideoPresetation() {
   return (
     <video   preload="auto" autoPlay muted playsInline controls>
-      <source src="/videos/MatchKora_video.mp4" type="video/mp4" />
+      <source src="/videos/MatchKora_video2.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   )

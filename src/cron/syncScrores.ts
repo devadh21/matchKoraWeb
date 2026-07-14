a2c45fa178dc12db0adfab7051e38a5c0b104e07
devadh21 <devadh21@gmail.com>
2026-07-14 18:23:41 +0100

    implement hourly score synchronization using cron job2

---
+++ src/cron/syncScrores.ts
@@ -0,0 +1,17 @@
+import cron from 'node-cron'
+import { syncWorldCupScores } from '@/lib/api' 
+
+
+// Schedule the cron job to run every hour (refresh scores evry hour)
+// '0 * * * *' means run at minute 0 of every hour
+
+cron.schedule("*/1 * * * *", async () => {
+  console.log("Running score sync...");
+
+  try {
+    const result = await syncWorldCupScores();
+    console.log(`Updated scores  matches`);
+  } catch (error) {
+    console.error("Cron job failed:", error);
+  }
+});
